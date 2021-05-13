eec70fc7b793e27343ec93dcc608d267a5404bc1
MaciejPenar <maciek4841@wp.pl>
2021-05-13 20:32:51 +0200

    poprawa formatowania

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -1,16 +1,12 @@
 
-for (let i=1; i<=100; i++)
-{
-    if (i%3==0)
-    {
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
         console.log(i + "Fizz");
     }
-    if (i%5==0)
-    {
+    if (i % 5 === 0) {
         console.log(i + "Buzz");
     }
-    if (i%3==0 && i%5==0)
-    {
-    console.log(i + "FizzBuzz");
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i + "FizzBuzz");
     }
 }
