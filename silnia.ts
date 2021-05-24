22d27b7561d59064290ab1238043ddf1c245ca0e
MaciejPenar <maciek4841@wp.pl>
2021-05-24 18:27:03 +0200

    poprawa formatowania silnia fibonacic

--- silnia.ts
+++ silnia.ts
@@ -2,9 +2,8 @@ function silnia(n) {
     if (n === 0) {
         return 1
     }
-    else{
-        return n*silnia(n-1);
+    else {
+        return n * silnia(n - 1);
     }
 }
-
 console.log(silnia(5))
